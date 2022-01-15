aaf025b727bef629174e446d75dd6471a91751b3
WhiteBlackGoose <wbg@angouri.org>
2022-01-15 22:38:25 +0300

    WIP

--- gen.ps1
+++ gen.ps1
@@ -29,14 +29,33 @@ function Request
         return $res
     }
 }
+function RequestArray
+{
+    [CmdletBinding()]
+    param(
+        [string] $Message
+    )
+    Write-Host $Message
+    $res = Read-Host
+    if ($res -eq "")
+    {
+        return $Default
+    }
+    else
+    {
+        return $res.Split()
+    }
+}
 
 Write-Host "Hey. This script generates .net library template! Answer a few questions and then the script will start."
 
-$libraryName = Request "Your whole library name?"
-$modules     = Request "List all modules"
-$libTarget   = Request "Enter target for your library (default: netstandard2.0)"                      "netstandard2.0"
-$exeTarget   = Request "Target framework for tests/samples/benchmarks/playground (default: net6.0):"  "net6.0"
-$testFw      = Request "Test framework (default: xUnit):"                                             "xunit"
+$libraryName = Request      "Your whole library name?"
+$authorName  = Request      "Author name (default: no name specified)"
+$modules     = RequestArray "List all modules"
+$libTarget   = Request      "Enter target for your library (default: netstandard2.0)"                      "netstandard2.0"
+$exeTarget   = Request      "Target framework for tests/samples/benchmarks/playground (default: net6.0):"  "net6.0"
+$testFw      = Request      "Test framework (default: xUnit):"                                             "xunit"
+$license     = Request      "License (default: MIT):"                                                      "MIT"
 
 dotnet new sln -n $LibraryName
 
@@ -47,6 +66,48 @@ New-Item -Name "Samples"     -ItemType "directory"
 New-Item -Name "Playground"  -ItemType "directory" 
 
 $folders = "Sources", "Tests", "Benchmarks", "Samples", "Playground"
+
+dotnet tool install dotnet-proj-cli --prerelease
+
+# Directory.Build sector start
+
+$dbp = "Directory.Build.props"
+$dbt = "Directory.Build.targets"
+
+Set-Location Sources
+dotnet proj create -o $dbp
+dotnet proj create -o $dbt
+
+dotnet proj add -o $dbp -p TargetFrameworks -v $libTarget
+if ($authorName -ne "")
+{
+    dotnet proj add -o $dbp -p Authors -v $authorName
+}
+dotnet proj add -o $dbp -p PackageLicenseExpression -v $license
+
+Set-Location ..
+
+Set-Location Tests
+dotnet proj create -o $dbp
+dotnet proj create -o $dbt
+Set-Location ..
+
+Set-Location Benchmarks
+dotnet proj create -o $dbp
+dotnet proj create -o $dbt
+Set-Location ..
+
+Set-Location Samples
+dotnet proj create -o $dbp
+dotnet proj create -o $dbt
+Set-Location ..
+
+Set-Location Playground
+dotnet proj create -o $dbp
+dotnet proj create -o $dbt
+Set-Location ..
+
+# Directory.Build sector end
 
 for ($i = 0; $i -lt $folders.Count; $i++)
 {
@@ -62,7 +123,7 @@ for ($i = 0; $i -lt $modules.Count; $i++)
     $sample     = $module + ".Sample"
     $playground = $module + ".Playground"
     $lang       = "C#"
-    $postfix    = ".csproj"
+    $postfix    = "csproj"
     
     Set-Location Sources
     dotnet new classlib -n $module -f $libTarget -lang $lang
@@ -84,14 +145,16 @@ for ($i = 0; $i -lt $modules.Count; $i++)
     dotnet new console -n $Playground -f $exeTarget -lang $lang
     Set-Location ..
     
-    dotnet add reference Tests/$test/$test.$postfix                  Sources/$module/$module.$postfix
-    dotnet add reference Benchmarks/$benchmark/$benchmark.$postfix   Sources/$module/$module.$postfix
-    dotnet add reference Samples/$sample/$sample.$postfix            Sources/$module/$module.$postfix
-    dotnet add reference Playground/$playground/$playground.$postfix Sources/$module/$module.$postfix
+    $sourceProj = "Sources/$module/$module.$postfix"
+
+    dotnet add "Tests/$test/$test.$postfix"                  reference $sourceProj
+    dotnet add "Benchmarks/$benchmark/$benchmark.$postfix"   reference $sourceProj
+    dotnet add "Samples/$sample/$sample.$postfix"            reference $sourceProj
+    dotnet add "Playground/$playground/$playground.$postfix" reference $sourceProj
     
-    dotnet sln add Sources/$module
-    dotnet sln add Tests/$test
-    dotnet sln add Benchmarks/$benchmark
-    dotnet sln add Samples/$sample
-    dotnet sln add Playground/$Playground
+    dotnet sln add "Sources/$module"
+    dotnet sln add "Tests/$test"
+    dotnet sln add "Benchmarks/$benchmark"
+    dotnet sln add "Samples/$sample"
+    dotnet sln add "Playground/$Playground"
 }
